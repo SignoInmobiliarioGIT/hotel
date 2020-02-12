b7b62b90e8ef9f3d38d2d79ee9f112a34ed93932
Alejandro Leone <dantofema@gmail.com>
2020-02-12 17:10:41 -0300

    add - Sale -mrc

---
+++ app/Http/Controllers/SaleGroupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SaleGroup;
+use Illuminate\Http\Request;
+
+class SaleGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SaleGroup  $saleGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SaleGroup $saleGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SaleGroup  $saleGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SaleGroup $saleGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SaleGroup  $saleGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SaleGroup $saleGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SaleGroup  $saleGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SaleGroup $saleGroup)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SaleSubGroupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SaleSubGroup;
+use Illuminate\Http\Request;
+
+class SaleSubGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SaleSubGroup  $saleSubGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SaleSubGroup $saleSubGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SaleSubGroup  $saleSubGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SaleSubGroup $saleSubGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SaleSubGroup  $saleSubGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SaleSubGroup $saleSubGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SaleSubGroup  $saleSubGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SaleSubGroup $saleSubGroup)
+    {
+        //
+    }
+}
